b2c8dcddd3045942a5cfd3259345de4464af0167
frankqiang <tyq155888@163.com>
2022-04-14 21:25:06 +0800

    css影响最小原则

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,6 +3,9 @@
   <h1>示例1</h1>
   <div>
     <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
   <div></div>
 </template>
@@ -11,6 +14,12 @@
 import Button from '../lib/Button.vue'
 export default {
   components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log('hi')
+    }
+    return { onClick }
+  },
 }
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import './lib/gulu.scss'
+import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.scss'
+
 import { router } from './router'
 
 
-const app=createApp(App)
+const app = createApp(App)
 app.use(router)
 app.mount('#app')
