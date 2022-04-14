b17e0459fb925b233856b4df9d1a0b79fea95d40
frankqiang <tyq155888@163.com>
2022-04-14 13:07:14 +0800

    Home组件的css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <topnav/>
+    <topnav />
     <div class="banner">
       <h1>咕噜UI</h1>
       <h2>一个厉害的UI框架</h2>
@@ -19,6 +19,27 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 .topnav {
   background: pink;
   display: flex;
