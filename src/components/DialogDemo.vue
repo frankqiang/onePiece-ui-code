b9bd867390979fded9bcea0796a198ae96623631
frankqiang <tyq155888@163.com>
2022-04-15 12:29:56 +0800

    dialog支持自定义title和content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,15 @@
   <div>Dialog示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :ok="confirm" :cancel="cancel" />
+  <Dialog v-model:visible="x" :ok="confirm" :cancel="cancel">
+    <template v-slot:content>
+      <div>hi</div>
+      <div>hi2</div>
+    </template>
+    <template v-slot:title>
+      <strong>加粗的标题</strong>
+    </template>
+  </Dialog>
 </template>
 
 <script>
