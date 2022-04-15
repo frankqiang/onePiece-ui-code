99591618cfeb35b9903aaf0c03250722d5078fb3
frankqiang <tyq155888@163.com>
2022-04-15 15:48:52 +0800

    背景渐变

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="topnav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-      <span class="toggleAside" @click="toggleAside"></span>
-    </div>
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+    <span class="toggleAside" @click="toggleAside"></span>
+  </div>
 </template>
 
 <script lang='ts'>
 import { inject, Ref } from 'vue'
 export default {
-  setup(){
-    const asideVisible=inject<Ref<boolean>>('asideVisible')
-    const toggleAside=()=>{
-      asideVisible.value=!asideVisible.value
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible')
+    const toggleAside = () => {
+      asideVisible.value = !asideVisible.value
     }
-    return {toggleAside}
-  }
+    return { toggleAside }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .topnav {
-  background: pink;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -47,7 +46,6 @@ export default {
     }
   }
 
-
   > .toggleAside {
     display: inline-block;
     width: 24px;
@@ -60,10 +58,16 @@ export default {
     display: none;
   }
 
-  @media (max-width:500px) {
-    > .menu{display: none;}
-    > .logo{margin: 0 auto;}
-    > .toggleAside{display: inline;}
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline;
+    }
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="topnavAndBanner">
     <topnav />
     <div class="banner">
       <h1>咕噜UI</h1>
@@ -15,21 +15,26 @@
 import Topnav from '../components/Topnav.vue'
 export default {
   components: { Topnav },
-  
 }
 </script>
 <style lang="scss" scoped>
+.topnavAndBanner {
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
+}
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
-  > .actions{
+  > .actions {
     padding: 8px 0;
     a {
-      margin:0 8px;
+      margin: 0 8px;
       background: #fff;
       display: inline-block;
       $h: 28px;
@@ -41,7 +46,6 @@ export default {
   }
 }
 .topnav {
-  background: pink;
   display: flex;
   padding: 16px;
   > .logo {
