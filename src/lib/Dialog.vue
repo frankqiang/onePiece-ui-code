bc263ee1c34adf4b9ce62b954472d5de919f4859
frankqiang <tyq155888@163.com>
2022-04-15 12:36:24 +0800

    使用teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,5 +1,6 @@
 <template>
   <template v-if="visible">
+    <teleport to='body' >
     <div class="gulu-dialog-overlay" @click="OnClickOverlay"></div>
     <div class="gulu-dialog-wrapper">
       <div class="gulu-dialog">
@@ -16,6 +17,7 @@
         </footer>
       </div>
     </div>
+    </teleport>
   </template>
 </template>
  
