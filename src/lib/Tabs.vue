ab3025a21c9d990586868f4b50eed3eced4b0328
frankqiang <tyq155888@163.com>
2022-04-15 13:23:46 +0800

    渲染tags内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>tabs组件</div>
+  <div>
+    <div v-for="(t,i) in titles" :key="i">{{t}}</div>
+    <component v-for="(c,i) in defaults" :is="c" :key="i"></component>
+  </div>
 </template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是Tab')
+      }
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    return { defaults, titles }
+  },
+}
+</script>
