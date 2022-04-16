9e50362c876e0e1655b30d93d94d76e9c0942899
frankqiang <tyq155888@163.com>
2022-04-16 14:27:26 +0800

    改造组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,7 @@
             <use xlink:href="#icon-ts" />
           </svg>
           <h3>基于 TypeScript</h3>
-          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+          <p>源代码采用 TypeScript 书写</p>
         </li>
         <li>
           <svg>
@@ -58,18 +58,28 @@ $border-radius: 4px;
 }
 .features {
   margin: 64px auto;
-  width: 400px;
+  padding: 0 16px;
+
   @media (min-width: 800px) {
     width: 800px;
+    > ul {
+      > li {
+        width: 50%;
+      }
+    }
   }
   @media (min-width: 1200px) {
     width: 1200px;
+    > ul {
+      > li {
+        width: 33.3333%;
+      }
+    }
   }
   > ul {
     display: flex;
     flex-wrap: wrap;
     > li {
-      width: 400px;
       margin: 16px 0;
       display: grid;
       justify-content: start;
@@ -101,7 +111,6 @@ $border-radius: 4px;
   align-items: center;
   flex-direction: column;
   color: $color;
-  font-size: 30px;
   > .actions {
     padding: 8px 0;
     a {
