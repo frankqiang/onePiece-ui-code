58951ed29ddbadaaca497b2d1847955cfa84ac98
“frankqiang <tyq155888@163.com>
2022-04-18 14:15:09 +0800

    更新deploy文件

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m 'update' &&
 git branch -M master &&
-git remote add origin git@github.com:frankqiang/onePiece-ui.git &&
+git remote add https://gitee.com/yiqiangtang/one-piece-ui.git &&
 git push -f -u origin master &&
 cd -
 echo https://frankqiang.github.io/onePiece-ui/index.html
