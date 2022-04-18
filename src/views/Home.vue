ce049b6f84aeab32f1cdb97f149425b74507f444
frankqiang <tyq155888@163.com>
2022-04-18 11:23:31 +0800

    优化首页手机端样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,8 @@
       <topnav />
       <div class="banner">
         <h1>草帽UI</h1>
-        <h2>一个厉害的UI框架</h2>
+        <h2>站在巨人的船上，扬帆起航</h2>
+
         <p class="actions">
           <a href="https://github">源码地址</a>
           <router-link to="/doc">开始使用</router-link>
@@ -57,6 +58,17 @@ $border-radius: 4px;
   clip-path: ellipse(80% 60% at 50% 40%);
   padding-bottom: 88px;
 }
+@media (max-width: 500px) {
+  .topnavAndBanner {
+    background: linear-gradient(
+      145deg,
+      rgba(227, 255, 253, 1) 0%,
+      rgba(183, 233, 230, 1) 100%
+    );
+    clip-path: ellipse(80% 60% at 50% 40%);
+    padding-bottom: 36px;
+  }
+}
 .features {
   margin: 64px auto;
   padding: 0 16px;
@@ -105,17 +117,24 @@ $border-radius: 4px;
     }
   }
 }
+@media (max-width: 500px) {
+  .features {
+    margin: 10px auto;
+    padding: 0 16px;
+  }
+}
 .banner {
-  padding: 100px 0;
+  padding: 90px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   color: $color;
+
   > .actions {
     padding: 8px 0;
     a {
-      margin: 0 8px;
+      margin: 30px 8px;
       background: $green;
       color: white;
       display: inline-block;
@@ -128,10 +147,41 @@ $border-radius: 4px;
     }
   }
 }
+@media (max-width: 500px) {
+  .banner {
+    padding: 100px 0 0 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: $color;
+
+    > .actions {
+      padding: 8px 0;
+      a {
+        margin: 8px;
+        background: $green;
+        color: white;
+        display: inline-block;
+        border-radius: $border-radius;
+        padding: 8px 24px;
+        font-size: 20px;
+        &:hover {
+          text-decoration: none;
+        }
+      }
+    }
+  }
+}
 .topnav {
   display: flex;
   padding: 16px 60px;
   box-shadow: 0 5px 5px rgb(51 51 51 / 10%);
+  background: linear-gradient(
+    145deg,
+    rgb(227, 255, 253) 0%,
+    rgb(183, 233, 230) 100%
+  );
   > .logo {
     max-width: 6em;
     margin-right: auto;
